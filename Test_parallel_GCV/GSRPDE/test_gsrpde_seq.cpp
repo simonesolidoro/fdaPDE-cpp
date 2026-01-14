018a57cb8c6c190f046c160305bb8bab54e57e01
simo <simone1.solidoro@mail.polimi.it>
2026-01-14 12:10:08 +0000

    cartella per test GSRPDE

---
+++ Test_parallel_GCV/GSRPDE/test_gsrpde_seq.cpp
@@ -0,0 +1,32 @@
+using namespace fdapde;
+using fdapde::test::almost_equal;
+
+// test 1
+//    mesh:         unit_square_40
+//    sampling:     locations != nodes
+//    penalization: simple laplacian
+//    covariates:   no
+//    BC:           no
+//    order FE:     1
+//    distribution: poisson
+int main() {
+    // geometry
+    std::string mesh_path = "../data/mesh/unit_square_40/";
+    Triangulation<2, 2> D(mesh_path + "points.csv", mesh_path + "elements.csv", mesh_path + "boundary.csv", true, true);
+    // data
+    GeoFrame data(D);
+    auto& l1 = data.insert_scalar_layer<POINT>("l1", "../data/gsr/01/locs.csv");
+    l1.load_csv<double>("../data/gsr/01/response.csv");
+    // physics
+    FeSpace Vh(D, P1<1>);
+    TrialFunction f(Vh);
+    TestFunction v(Vh);
+    auto a = integral(D)(dot(grad(f), grad(v)));
+    ZeroField<2> u;
+    auto F = integral(D)(u * v);
+    // modeling
+    GSRPDE m("y ~ f", data, fdapde::poisson_distribution(), fe_ls_elliptic_gsr(a, F));
+    m.fit(/* lambda = */0, 1.25e-06);
+
+    EXPECT_TRUE(almost_equal<double>(m.f(), "../data/gsr/01/field.mtx"));
+}
